484c496f1195c63696e8f6c77556e9cf839a853c
bandvov <bandvov@gmail.com>
2020-12-24 23:01:20 +0200

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  generateBuildId: async () => "001",
+  generateBuildId: async () => '001',
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
 
+    return config;
+  },
 };
