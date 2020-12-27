d454a61067d664d3381a2e3db4978e272e0feed5
bandvov <bandvov@gmail.com>
2020-12-27 13:04:00 +0200

    added out

---
+++ out/_next/static/chunks/pages/_error-169ac5fbca9ff4515195.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[10],{"1HF/":function(n,o,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return r("Y0NT")}])},Y0NT:function(n,o,r){"use strict";r.r(o),r.d(o,"default",(function(){return _}));var t=r("nKUr");function _(){return Object(t.jsx)("h1",{children:"oops something went wrong"})}}},[["1HF/",0,2,1]]]);
