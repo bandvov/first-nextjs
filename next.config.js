e2cdae2023c56e91af13ed13441eb81b04e6718f
bandvov <48312647+bandvov@users.noreply.github.com>
2021-01-01 21:57:30 +0200

    Revert "added .next folder"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const isProd = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  basePath: isProd ? '' : '/first-nextjs',
+  basePath: isProd ? '/first-nextjs' : '',
   generateBuildId: async () => '001',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
