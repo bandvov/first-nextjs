b13527b4f370b4c25daf26b3278b5ab5149cef90
bandvov <48312647+bandvov@users.noreply.github.com>
2021-01-09 19:29:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
-  basePath: isDev ? '' : '/first-nextjs',
   generateBuildId: async () => '001',
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
