771d6327d5292c93b47997dea4f5eed8e140f7b7
bandvov <48312647+bandvov@users.noreply.github.com>
2021-03-19 22:43:18 +0200

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
     export const client = new ApolloClient({
-  uri: `http://localhost:3000/api/graphql`,
+  uri: `${process.env.PATH}/api/graphql`,
   cache: new InMemoryCache({ addTypename: false }).restore({}),
 });
