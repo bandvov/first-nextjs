179a7af23c2fc3e022aa539681444afd67bd91d3
bandvov <bandvov@gmail.com>
2021-03-19 19:33:39 +0200

    :recycle:

--- utils/client.js
+++ utils/client.js
@@ -2,8 +2,7 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const PORT = process.env.PORT || 3000;
-const path = process.env.ENV_NODE ==='development'?`http://localhost:${PORT}`:'https://vcars.herokuapp.com';
-
+const path = process.env.ENV ==='development'?`http://localhost:${PORT}`:'https://vcars.herokuapp.com';
 
 export const client = new ApolloClient({
   uri: `${path}/api/graphql`,
