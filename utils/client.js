af40d48e98c80cca26ab90cb489e8f269cfa4920
bandvov <48312647+bandvov@users.noreply.github.com>
2021-03-20 07:57:53 +0200

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
     export const client = new ApolloClient({
-  uri: `http://localhost:3000/api/graphql`,
+  uri: `https://vcars.herokuapp.com/api/graphql`,
   cache: new InMemoryCache({ addTypename: false }).restore({}),
 });
