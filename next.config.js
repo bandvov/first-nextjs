ac47b850587855bf6c97bd3ec4888a76b7c374c2
bandvov <48312647+bandvov@users.noreply.github.com>
2021-03-24 09:51:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-  generateBuildId: async () => "001",
-  async headers() {
-    return [
-      {
-        source: "/api/[[...slug]]",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
-  },
+  generateBuildId: async () => "001"
 };
